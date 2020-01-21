a78c7e0f64c343ce59debefcc89c4bdac3f5cd49
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2020-01-21 17:42:50 +0530

    [Shalinee Bhawnani] Clean up ignored files

--- FundooNotesApp/myproject/myproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import login, register
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/login', login),
-    path('api/register', register)
-]
--- README.md/readme.md
+++
@@ -1,1 +0,0 @@
-Creating a simple sing up view and then moving onto more advanced sign up view with profile model and confirmation mail
