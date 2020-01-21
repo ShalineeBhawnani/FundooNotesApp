8bf1af0fd47f97d54b5b87cd9c24c82b4af698a0
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2020-01-21 15:56:25 +0530

    [Shalinee Bhawnani]Implements implemented redis cache in FundooLoginPage

--- FundooLoginPage/project/project/settings.py
+++ FundooLoginPage/project/project/settings.py
@@ -138,20 +138,10 @@ STATIC_URL = '/static/'
 #     ),
 # }
 
-# EMAIL_BACKEND = ‘django.core.mail.backends.smtp.EmailBackend’
-# EMAIL_HOST = ‘smtp.gmail.com’
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = ‘your_account@gmail.com’
-# EMAIL_HOST_PASSWORD = ‘your account’s password’
-# EMAIL_USE_TLS = True
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ('shalineebhawnani80@gmail.com')
-EMAIL_HOST_PASSWORD = ('ShalineeShalu5@5@')
+MAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD os.getenv('EMAIL_HOST_PASSWORD')
 
 
 JWT_AUTH = {
