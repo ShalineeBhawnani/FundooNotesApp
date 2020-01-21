95e3f895ba19087c0dd956a7d6f567845b283e3a
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2020-01-21 18:17:02 +0530

    [Shalinee Bhawnani]Add.added Test.py in FundooLoginPage

--- FundooLoginPage/project/tests/test_urls.py
+++ FundooLoginPage/project/tests/test_urls.py
@@ -1,10 +1,20 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from snippets.views import Login, Registrations
-
+from snippets.views import Login, Registrations, ForgotPassword
 
 class TestUrls(SimpleTestCase):
     def test_login(self):
         url = reverse('login')
         print(resolve(url))
         self.assertEquals(resolve(url).func.view_class, Login)
+
+    def test_registration(self):
+        url = reverse('registration')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, Registrations)
+
+    def test_forgot_Password(self):
+        url = reverse('forgot_Password')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, ForgotPassword)
+
