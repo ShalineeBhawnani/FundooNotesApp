fd3db8168a3d04b7b22dc2c65b26b8749a37b6f5
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2020-01-25 18:15:19 +0530

    [Shalinee Bhawnani]Refractor.refracted S3MediaExample

--- S3MediaExample/mysite/settings.py
+++ S3MediaExample/mysite/settings.py
@@ -92,17 +92,24 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mysite/static'),
 ]
 
-AWS_ACCESS_KEY_ID = ''
-AWS_SECRET_ACCESS_KEY = ''
-AWS_STORAGE_BUCKET_NAME = 'mediastoragebucket'
+AWS_ACCESS_KEY_ID = 'AKIA47E3ZD5TXMUNFDHQ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'django-private-assets'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
-AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+AWS_STATIC_LOCATION = 'static'
+STATICFILES_STORAGE = 'mysite.storage_backends.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
+AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
+DEFAULT_FILE_STORAGE = 'mysite.storage_backends.PublicMediaStorage'
+
+AWS_PRIVATE_MEDIA_LOCATION = 'media/private'
+PRIVATE_FILE_STORAGE = 'mysite.storage_backends.PrivateMediaStorage'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'profile'
