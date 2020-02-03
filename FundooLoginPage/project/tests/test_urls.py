a64fc64ef8217407fb25fb4bf7f32c741ebc1130
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2020-02-03 15:52:54 +0530

    [Shalinee Bhawnani]Added.add Test File For Note App

--- FundooLoginPage/project/tests/test_urls.py
+++ FundooLoginPage/project/tests/test_urls.py
@@ -1,6 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from snippets.views import Login, Registrations, ForgotPassword
+from note.views import NoteList,LabelList
 
 class TestUrls(SimpleTestCase):
     def test_login(self):
@@ -13,8 +14,19 @@ class TestUrls(SimpleTestCase):
         print(resolve(url))
         self.assertEquals(resolve(url).func.view_class, Registrations)
 
-    def test_forgot_Password(self):
-        url = reverse('forgot_Password')
+    def test_forgotPassword(self):
+        url = reverse('forgotPassword')
         print(resolve(url))
         self.assertEquals(resolve(url).func.view_class, ForgotPassword)
+
+    def test_note(self):
+        url = reverse('note')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, NoteList)
+    
+
+    def test_label(self):
+        url = reverse('label')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, LabelList)
 
